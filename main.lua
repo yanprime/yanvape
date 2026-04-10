48dd55043ede379a861b977542b2c08eb069a478
yanprime <yantheprime@gmail.com>
2026-04-09 22:07:11 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -46,7 +46,7 @@ local function downloadFile(path, func)
         local success = false
         for attempt = 1, 3 do
             local suc, result = pcall(function()
-                return game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/' .. readfile('newvape/profiles/commit.txt') .. '/' .. select(1, path:gsub('newvape/', '')), true)
+                return game:HttpGet('https://raw.githubusercontent.com/yanprime/yanvape/' .. readfile('newvape/profiles/commit.txt') .. '/' .. select(1, path:gsub('newvape/', '')), true)
             end)
             if suc and result ~= '404: Not Found' then
                 res = result
@@ -85,7 +85,7 @@ local function finishLoading()
                 repeat task.wait() until game:IsLoaded()
                 if getgenv and not getgenv().shared then getgenv().shared = {} end
                 shared.vapereload = true
-                loadstring(game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+                loadstring(game:HttpGet('https://raw.githubusercontent.com/yanprime/yanvape/'..readfile('newvape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
             ]]
 			if shared.VapeDeveloper then
 				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
@@ -167,7 +167,7 @@ if not shared.VapeIndependent then
     else
         if not shared.VapeDeveloper then
             local suc, res = pcall(function()
-                return game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/' .. readfile('newvape/profiles/commit.txt') .. '/games/' .. gameFileId .. '.lua', true)
+                return game:HttpGet('https://raw.githubusercontent.com/yanprime/yanvape/' .. readfile('newvape/profiles/commit.txt') .. '/games/' .. gameFileId .. '.lua', true)
             end)
             if suc and res ~= '404: Not Found' then
                 loadstring(downloadFile('newvape/games/' .. gameFileId .. '.lua'), tostring(gameFileId))(...)
